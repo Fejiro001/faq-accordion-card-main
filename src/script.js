d07a195417930dc7106f0101899b62adf1cd0903
Abere Fejiro <aberefejiro@gmail.com>
2024-03-28 22:56:56 +0100

    styling of elements complete

--- src/script.js
+++ src/script.js
@@ -1,1 +1,8 @@
 "use strict";
+const accordionButtons = document.querySelectorAll(".accordion-button");
+
+accordionButtons.forEach((accordionButton) => {
+  accordionButton.addEventListener("click", function () {
+    console.log("clicked");
+  });
+});
