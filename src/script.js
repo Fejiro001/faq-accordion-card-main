2cd6f4ab5f5e7f7ce17b8d2d75148548305f3e88
Abere Fejiro <aberefejiro@gmail.com>
2024-03-29 22:28:55 +0100

    IMPROVE PERFORMANCE

--- src/script.js
+++ src/script.js
@@ -1,11 +1,9 @@
 "use strict";
 const accordionButtons = document.querySelectorAll(".accordion-button");
-
 accordionButtons.forEach((accordionButton) => {
   accordionButton.addEventListener("focus", function () {
     accordionButton.setAttribute("aria-expanded", true);
   });
-
   accordionButton.addEventListener("blur", function () {
     accordionButton.setAttribute("aria-expanded", false);
   });
