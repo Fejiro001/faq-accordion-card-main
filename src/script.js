0b1e8a473f8f1f7e544b0f17243330ff8da48910
Abere Fejiro <aberefejiro@gmail.com>
2024-03-29 22:17:02 +0100

    functionality to toggle value of aria-expanded

--- src/script.js
+++ src/script.js
@@ -1,10 +1,12 @@
 "use strict";
 const accordionButtons = document.querySelectorAll(".accordion-button");
-const accordionPanels = document.querySelectorAll(".accordion-panel");
-// console.log(accordionPanels);
 
 accordionButtons.forEach((accordionButton) => {
-  accordionButton.addEventListener("click", function () {
-    // console.log("clicked");
+  accordionButton.addEventListener("focus", function () {
+    accordionButton.setAttribute("aria-expanded", true);
+  });
+
+  accordionButton.addEventListener("blur", function () {
+    accordionButton.setAttribute("aria-expanded", false);
   });
 });
